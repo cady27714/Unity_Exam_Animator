4b25422c43cda9a90da5acd63d62e56cdfd943ff
cady27714 <54615342+cady27714@users.noreply.github.com>
2019-11-07 10:37:58 +0800

    動態捕捉_B1040432_郭又慈

---
+++ UnityProject/Assets/模型/ani.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ani : MonoBehaviour
+{
+    private Animator A;
+    // Start is called before the first frame update
+    void Start()
+    {
+        A = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            A.SetTrigger("受傷觸發");
+            A.SetBool("跑步開關", Input.GetKey(KeyCode.R));
+        }
+    }
+}
